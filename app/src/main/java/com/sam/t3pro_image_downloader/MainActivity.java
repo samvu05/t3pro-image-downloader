718742506c09b31639698107ddde90afd7419c01
samvu.05 <samvu.it@gmail.com>
2020-04-26 23:46:32 +0700

    add project (not complete)

---
+++ app/src/main/java/com/sam/t3pro_image_downloader/MainActivity.java
@@ -0,0 +1,14 @@
+package com.sam.t3pro_image_downloader;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+    }
+}
