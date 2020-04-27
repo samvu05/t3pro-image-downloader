22f0ddbad2aee035a34448939c232d5baa3161ee
samvu.05 <samvu.it@gmail.com>
2020-04-27 15:02:35 +0700

    take break, learn Driver subject

--- app/src/main/java/com/sam/t3pro_image_downloader/EditActivity.java
+++ app/src/main/java/com/sam/t3pro_image_downloader/EditActivity.java
@@ -1,4 +1,104 @@
 package com.sam.t3pro_image_downloader;
 
-public class EditActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EditActivity extends AppCompatActivity{
+    private Button btnSubmit;
+    private EditText edt1;
+    private EditText edt2;
+    private EditText edt3;
+    private EditText edt4;
+    private EditText edt5;
+    private Button btn1;
+    private Button btn2;
+    private Button btn3;
+    private Button btn4;
+    private Button btn5;
+
+    public static final String LINK1 = "link1";
+    public static final String LINK2 = "link2";
+    public static final String LINK3 = "link3";
+    public static final String LINK4 = "link4";
+    public static final String LINK5 = "link5";
+    public static final String BUNDLE = "bundle";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.edit);
+        getWidget();
+        setEvent();
+    }
+    protected void getWidget() {
+        edt1 = findViewById(R.id.edt_1);
+        edt2 = findViewById(R.id.edt_2);
+        edt3 = findViewById(R.id.edt_3);
+        edt4 = findViewById(R.id.edt_4);
+        edt5 = findViewById(R.id.edt_5);
+        btn1 = findViewById(R.id.btn_clear1);
+        btn2 = findViewById(R.id.btn_clear2);
+        btn3 = findViewById(R.id.btn_clear3);
+        btn4 = findViewById(R.id.btn_clear4);
+        btn5 = findViewById(R.id.btn_clear5);
+        btnSubmit = findViewById(R.id.btn_submit);
+    }
+    protected void setEvent() {
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edt1.setText("");
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edt2.setText("");
+            }
+        });
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edt3.setText("");
+            }
+        });
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edt4.setText("");
+            }
+        });
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edt5.setText("");
+            }
+        });
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                passLinkData();
+            }
+        });
+    }
+    public void passLinkData() {
+        Intent intent = new Intent(EditActivity.this,HomeActivity.class);
+        Bundle bundle = new Bundle();
+
+        bundle.putString(LINK1,edt1.getText().toString());
+        bundle.putString(LINK2,edt2.getText().toString());
+        bundle.putString(LINK3,edt3.getText().toString());
+        bundle.putString(LINK4,edt4.getText().toString());
+        bundle.putString(LINK5,edt5.getText().toString());
+
+        intent.putExtra(BUNDLE,bundle);
+        startActivity(intent);
+    }
 }
--- app/src/main/java/com/sam/t3pro_image_downloader/HomeActivity.java
+++ app/src/main/java/com/sam/t3pro_image_downloader/HomeActivity.java
@@ -2,13 +2,73 @@ package com.sam.t3pro_image_downloader;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
-
+    private ImageButton btnEdit;
+    private ImageView imgv1;
+    private ImageView imgv2;
+    private ImageView imgv3;
+    private ImageView imgv4;
+    private ImageView imgv5;
+    private String linkImg1 = "";
+    private String linkImg2 = "";
+    private String linkImg3 = "";
+    private String linkImg4 = "";
+    private String linkImg5 = "";
+    private TextView textView;
+    private int count = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
+        getWidget();
+        btnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this,EditActivity.class);
+                startActivity(intent);
+                count++;
+            }
+        });
+        if(count != 0){
+            getDataFromEditActivity();
+        }
+    }
+
+    private void getWidget() {
+        imgv1 = findViewById(R.id.iv_img1);
+        imgv2 = findViewById(R.id.iv_img2);
+        imgv3 = findViewById(R.id.iv_img3);
+        imgv4 = findViewById(R.id.iv_img4);
+        imgv5 = findViewById(R.id.iv_img5);
+        btnEdit = findViewById(R.id.btn_edit);
+        textView = findViewById(R.id.tempa);
+    }
+
+    private void getDataFromEditActivity() {
+        Intent intent = getIntent();
+        Bundle bundle = intent.getBundleExtra(EditActivity.BUNDLE);
+        String link1 = bundle.getString(EditActivity.LINK1);
+        String link2 = bundle.getString(EditActivity.LINK2);
+        String link3 = bundle.getString(EditActivity.LINK3);
+        String link4 = bundle.getString(EditActivity.LINK4);
+        String link5 = bundle.getString(EditActivity.LINK5);
+
+        textView.setText(link1);
+
+//        Toast.makeText(HomeActivity.this,link1,Toast.LENGTH_SHORT).show();
+//        Toast.makeText(HomeActivity.this,link2,Toast.LENGTH_SHORT).show();
+//        Toast.makeText(HomeActivity.this,link3,Toast.LENGTH_SHORT).show();
+//        Toast.makeText(HomeActivity.this,link4,Toast.LENGTH_SHORT).show();
+//        Toast.makeText(HomeActivity.this,link5,Toast.LENGTH_SHORT).show();
     }
 }
